46ad0db28f5c0134bf2b24890a8fc970c8dd3704
Jonathan Cardoso Lopes Domingos <jonathan.domingos@sondait.com.br>
2020-09-27 17:43:26 -0300

    configuracao de senhas

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ if(process.env.NODE_ENV == "production"){
     /*
         DATA-BASE (PROD)
     */
-    module.exports = {mongoURI: "mongodb+srv://jonathan:1246583dv7@cluster0.5ueib.mongodb.net/auth-project?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "string de conexão do mongoDB PROD"}
 }else{
     /*
         DATA-BASE (DEV)
     */
-    module.exports = {mongoURI: "mongodb+srv://jonathan:1246583dv7@cluster0.5ueib.mongodb.net/auth-project?retryWrites=true&w=majority"}
+    module.exports = {mongoURI: "string de conexão do mongoDB DEV "}
 }
